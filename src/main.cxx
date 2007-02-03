d01f71789de24f63af80fc296f6e4d81844cff80
Martin Koller <kollix@aon.at>
2007-02-03 19:37:17 +0000

    correct copyright + version

--- src/main.cxx
+++ src/main.cxx
@@ -20,8 +20,8 @@
 int main(int argc, char **argv)
 {
   KAboutData about("kbackup", I18N_NOOP("KBackup"),
-                   "0.5.1", I18N_NOOP("An easy to use backup program"), KAboutData::License_GPL_V2,
-                   "(c) 2006, Martin Koller",  // copyright
+                   "0.5.2", I18N_NOOP("An easy to use backup program"), KAboutData::License_GPL_V2,
+                   "(c) 2006, 2007, Martin Koller",  // copyright
                    0,  // added text
                    "http://www.kde-apps.org/content/show.php?content=44998",  // homepage
                    "m.koller@surfeu.at");  // bugs to
