7fd0ec6407bb23c1438184100e0d9eb09ff27f53
Martin Koller <kollix@aon.at>
2018-01-05 11:30:48 +0100

    also match header files (.hxx)

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >rc.cpp
-$XGETTEXT `find . -name \*.h -o -name \*.cxx -o -name \*.cpp` -o $podir/kbackup.pot
+$XGETTEXT `find . -name \*.h -o -name \*.\?xx -o -name \*.cpp` -o $podir/kbackup.pot
 rm -f rc.cpp
