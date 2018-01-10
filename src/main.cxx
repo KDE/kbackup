3a47eab54386ae2a8dbe103d12ad5189ec908c10
Martin Koller <kollix@aon.at>
2018-01-10 19:57:17 +0100

    remove setTranslator() call, avoid crash on close without SysTray Icon

--- src/main.cxx
+++ src/main.cxx
@@ -16,6 +16,7 @@
 #include <QFile>
 #include <QString>
 #include <QTimer>
+#include <QPointer>
 
 #include <KAboutData>
 #include <KLocalizedString>
@@ -51,7 +52,6 @@ int main(int argc, char **argv)
                    "kollix@aon.at");  // bugs to
 
   about.addAuthor(i18n("Martin Koller"), i18n("Developer"), "kollix@aon.at");
-  about.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
 
   about.setOrganizationDomain(QByteArray("kde.org"));
   about.setDesktopFileName(QStringLiteral("org.kde.kbackup"));
@@ -98,7 +98,7 @@ int main(int argc, char **argv)
     KAboutData::setApplicationData(about);
   }
 
-  MainWindow *mainWin = 0;
+  QPointer<MainWindow> mainWin;
 
   if ( interactive )
   {
